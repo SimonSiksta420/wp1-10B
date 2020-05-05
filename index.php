9a4759e57bb5d6018e6f35a4cf748d37e16948bd
SimonSiksta420 <simon.siksta@student.ossp.cz>
2020-05-05 10:04:41 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,8 @@
     <title>Document</title>
 </head>
 <body>
-   <?php
-
-   for ($i=0; $i <= 400; $i+=20) { ?>
+   👺
+   <?php for ($i=0; $i <= 400; $i+=20) { ?>
        <div style="display: block; width: <?=$i?>px; height: 20px; background-color: #454d59;"> </div>
   <?php } ?>
 
